dd74b828e363d71df61a0588bdbc74b7efede39e
sungQ <sungkyulee8@gmail.com>
2023-12-08 16:52:45 +0900

    중요 코드 주석처리

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,9 +9,11 @@ import Input from "../Input/Input";
 
 import "./Chat.css";
 
-const ENDPOINT = "http://175.114.130.12:5000"; // 서버를 돌리는 곳의 IP주소
+// 서버의 IP 주소
+const ENDPOINT = "http://175.114.130.12:5000";
 
 const Chat = ({ location }) => {
+  // 상태 변수들
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
   const [users, setUsers] = useState("");
@@ -19,54 +21,69 @@ const Chat = ({ location }) => {
   const [messages, setMessages] = useState([]);
   const [socket, setSocket] = useState(null);
 
+  // 컴포넌트가 처음 마운트될 때 실행되는 효과
   useEffect(() => {
+    // URL에서 name과 room을 가져옵니다.
     const { name, room } = queryString.parse(window.location.search);
 
+    // 콘솔에 name과 room 출력
     console.log(name, room);
 
+    // 새로운 소켓 인스턴스 생성
     const newSocket = io(ENDPOINT);
     setSocket(newSocket);
 
+    // name과 room을 상태 변수에 설정
     setRoom(room);
     setName(name);
 
+    // 서버에 join 이벤트를 전송
     newSocket.emit("join", { name, room }, (error) => {
       if (error) {
         alert(error);
       }
     });
 
+    // 컴포넌트가 언마운트될 때 소켓 연결 해제
     return () => {
       newSocket.disconnect();
     };
   }, [ENDPOINT, window.location.search]);
 
+  // 메시지 및 사용자 업데이트를 처리하는 효과
   useEffect(() => {
+    // 소켓이 없으면 종료
     if (!socket) return;
 
+    // 서버로부터 메시지 이벤트를 수신하면 메시지 업데이트
     socket.on("message", (message) => {
       setMessages((messages) => [...messages, message]);
     });
 
+    // 서버로부터 roomData 이벤트를 수신하면 사용자 업데이트
     socket.on("roomData", ({ users }) => {
       setUsers(users);
     });
 
+    // 컴포넌트가 언마운트될 때 이벤트 리스너 해제
     return () => {
       socket.off("message");
       socket.off("roomData");
     };
   }, [socket]);
 
+  // 메시지를 전송하는 함수
   const sendMessage = (event) => {
     event.preventDefault();
 
+    // 메시지가 비어 있지 않으면 소켓을 통해 sendMessage 이벤트 전송
     if (message) {
       console.log(message);
       socket.emit("sendMessage", message, () => setMessage(""));
     }
   };
 
+  // 채팅 컴포넌트 렌더링
   return (
     <div className="outerContainer">
       <div className="container">
--- server/index.js
+++ server/index.js
@@ -8,10 +8,14 @@ const router = require("./router");
 // index.js
 const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
+// 서버 포트 설정
 const PORT = process.env.PORT || 5000;
 
+// Express 애플리케이션 및 HTTP 서버 생성
 const app = express();
 const server = http.createServer(app);
+
+// Socket.IO 서버 생성 및 CORS 설정
 const io = socketio(server, {
   cors: {
     origin: "*", // 허용할 도메인
@@ -19,29 +23,43 @@ const io = socketio(server, {
   },
 });
 
+// CORS 미들웨어 및 라우터 등록
 app.use(cors());
 app.use(router);
+
+// Socket.IO 이벤트 처리
 io.on("connect", (socket) => {
   console.log("새로운 유저가 접속했습니다.");
+
+  // join 이벤트 처리
   socket.on("join", ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
     if (error) callback({ error: "에러가 발생했습니다." });
 
+    // 새로운 유저를 방에 추가하고 환영 메시지 전송
     socket.emit("message", {
       user: "관리자",
       text: `${user.name}님 ${user.room}에 오신 것을 환영합니다.`,
     });
 
+    // 방에 속한 모든 유저에게 업데이트된 방 정보 전송
     io.to(user.room).emit("roomData", {
       room: user.room,
       users: getUsersInRoom(user.room),
     });
+
+    // 소켓을 방에 조인
     socket.join(user.room);
+
+    // 콜백 함수 호출
     callback();
   });
+
+  // sendMessage 이벤트 처리
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
 
+    // 유효한 유저가 존재하면 해당 방에 메시지 전송
     if (user && user.name) {
       io.to(user.room).emit("message", {
         user: user.name,
@@ -51,10 +69,15 @@ io.on("connect", (socket) => {
       console.error("Invalid user or user name is undefined:", user);
     }
 
+    // 콜백 함수 호출
     callback();
   });
+
+  // disconnect 이벤트 처리
   socket.on("disconnect", () => {
     const user = removeUser(socket.id);
+
+    // 유저가 퇴장하면 해당 방에 메시지 및 업데이트된 방 정보 전송
     if (user) {
       io.to(user.room).emit("message", {
         user: "관리자",
@@ -65,11 +88,12 @@ io.on("connect", (socket) => {
         users: getUsersInRoom(user.room),
       });
     }
+
     console.log("유저가 나갔습니다.");
   });
 });
 
+// 서버 시작 및 포트 출력
 server.listen(PORT, "0.0.0.0", () =>
-  // 접근 권한 해제
-  console.log(`서버가 ${PORT} 에서 시작되었습니다`)
+  console.log(`서버가 ${PORT}에서 시작되었습니다.`)
 );
